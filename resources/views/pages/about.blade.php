a47529e9f5135878bfa10c05df304b7d34a08b79
DHEHIBA <yacine.dhehiba@gmail.com>
2022-04-04 13:56:44 +0200

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Abou Us | Hello City</title>
-       
-    </head>
-    <body >
+@section('content')
         <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
         <p><a href="/">Revenir à la page d'accueil</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{ date('Y')}} &middot; </p>
-        </footer>
-    </body>
-</html>
+        
+    
+@endsection 
